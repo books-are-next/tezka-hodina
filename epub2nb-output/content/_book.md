93d6444db66a970cdf6e743e3dc198be32bc90c8
adelaweidischova <48671822+adelaweidischova@users.noreply.github.com>
2022-01-24 16:14:43 +0000

    Deploying to docs from @ books-are-next/tezka-hodina@1f3844c0dd58ac8bd6cc41155473307a9d4af0f6 🚀

--- epub2nb-output/content/_book.md
+++ epub2nb-output/content/_book.md
@@ -37,8 +37,92 @@ readingOrder:
   - colophon.html
 tocBase:
   - id: section-1
-    children: []
+    children:
+      - link: section0002.html
+        title: TĚŽKÁ HODINA
+        hiddenTitle: false
+      - link: section0004.html
+        title: BALADA O NENAROZENÉM DÍTĚTI
+        hiddenTitle: false
+      - link: section0005.html
+        title: |
+          SLEPÍ MUZIKANTI
+        hiddenTitle: false
+      - link: section0006.html
+        title: |
+          ČEPOBITÍ
+        hiddenTitle: false
+      - link: section0007.html
+        title: |
+          TVÁŘ ZA SKLEM
+        hiddenTitle: false
+      - link: section0008.html
+        title: |
+          KÁZÁNÍ NA HOŘE
+        hiddenTitle: false
+      - link: section0009.html
+        title: |
+          OČI
+        hiddenTitle: false
+      - link: section0010.html
+        title: |
+          JARO
+        hiddenTitle: false
+      - link: section0011.html
+        title: |
+          BÁSEŇ MILOSTNÁ
+        hiddenTitle: false
+      - link: section0012.html
+        title: |
+          BALADA O SNU
+        hiddenTitle: false
+      - link: section0013.html
+        title: |
+          SLOKY
+        hiddenTitle: false
+      - link: section0014.html
+        title: POHŘEB
+        hiddenTitle: false
+      - link: section0015.html
+        title: MUŽ
+        hiddenTitle: false
+      - link: section0016.html
+        title: |
+          FOTOGRAFIE
+        hiddenTitle: false
+      - link: section0017.html
+        title: |
+          DŮM V NOCI
+        hiddenTitle: false
+      - link: section0018.html
+        title: SETKÁNÍ
+        hiddenTitle: false
+      - link: section0019.html
+        title: |
+          MIROGOJ
+        hiddenTitle: false
+      - link: section0020.html
+        title: |
+          NĚVĚRNÁ
+        hiddenTitle: false
+      - link: section0021.html
+        title: |
+          ODJEZD
+        hiddenTitle: false
+      - link: section0022.html
+        title: |
+          MILENCI
+        hiddenTitle: false
+      - link: section0023.html
+        title: |
+          BALADA O OČÍCH TOPIČOVÝCH
+        hiddenTitle: false
+      - link: section0024.html
+        title: |
+          MOŘE
+        hiddenTitle: false
     isSection: true
+    listType: plain
 static:
   - style
   - scripts
--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 /* global self, caches, fetch */
 
-const CACHE = 'cache-8dda1e4';
+const CACHE = 'cache-1f3844c';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
@@ -40,7 +40,7 @@ self.addEventListener('activate', event => {
 });
 
 function precache() {
-  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./section0001.html","./section0002.html","./section0004.html","./section0005.html","./section0006.html","./section0007.html","./section0008.html","./section0009.html","./section0010.html","./section0011.html","./section0012.html","./section0013.html","./section0014.html","./section0015.html","./section0016.html","./section0017.html","./section0018.html","./section0019.html","./section0020.html","./section0021.html","./section0022.html","./section0023.html","./section0024.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/88x31.png","./resources/obalka.jpg","./resources/pd-88x31.png","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]));
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./section0001.html","./section0002.html","./section0004.html","./section0005.html","./section0006.html","./section0007.html","./section0008.html","./section0009.html","./section0010.html","./section0011.html","./section0012.html","./section0013.html","./section0014.html","./section0015.html","./section0016.html","./section0017.html","./section0018.html","./section0019.html","./section0020.html","./section0021.html","./section0022.html","./section0023.html","./section0024.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/88x31.png","./resources/obalka.jpg","./resources/pd-88x31.png","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]));
 }
 
 self.addEventListener('fetch', e => {
